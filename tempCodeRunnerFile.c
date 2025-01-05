90e14dbdd9f07d5f3601a63336ab576b7d398ebd
JABIR IMTEYAZ <knock5jabir@gmail.com>
2025-01-05 20:25:15 +0530

    Day1

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main (){
+    int a; // variable decl
+    a = 6; // variable initialization
+    // int a = 6;
+    printf("%d", a);
+    return 0;
+}
+
+#include<stdio.h>
+ int main (){
+    int a;
+    scanf("%d",&a);;
+    printf("The value of a is %d \n", a);
+    return 0;
+    
